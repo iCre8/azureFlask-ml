787be3668e5ba225d1cc09576b0bac6ab9037708
Roberto Thomas <RTHOMAS@silganplastics.com>
2020-12-21 15:27:03 -0600

    updated application name to flask-ml-demo

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-app.azurewebsites.net:$PORT/predict
+     -X POST https://flask-ml-demo.azurewebsites.net:$PORT/predict

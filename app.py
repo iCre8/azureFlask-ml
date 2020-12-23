97101a25ebee7908c2289985d96f7b335c5c1bfe
R. Thomas <find.rob@outlook.com>
2020-12-23 01:11:15 +0000

    update app.py homepage

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Hi From GitHub + AzurePipeline Deploy Sklearn Prediction Home</h3>"
+    html = "<h3> Prediction Home</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value

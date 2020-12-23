0f564dce29cbe8a58bc6986489c9cd1ec19c6b6f
Roberto Thomas <RTHOMAS@silganplastics.com>
2020-12-23 14:28:40 -0600

    removed test route for locust and app.py

--- locustfile.py
+++ locustfile.py
@@ -2,15 +2,9 @@ from locust import HttpUser, task, between
 
 
 class index_loaded(HttpUser): 
-    wait_time = between(5, 15)
+    wait_time = between(1, 5)
 
     def on_start(self): 
         self.client.get("https://flask-ml-demo.azurewebsites.net")
 
-    @task
-    def prediction(self):
-        self.client.post("/secondary_router")
-
-    @task
-    def run_predict(self): 
-        self.client.get("/predict")
+ 
